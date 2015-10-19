ca16138be013576b96d9635feb8d451f8fbed8c6
Balazs Bonis <b.bonis@mito.hu>
2015-10-19 23:10:38 +0200

    excel dialect

--- riwo/dialects/excel.py
+++ riwo/dialects/excel.py
@@ -1,5 +1,4 @@
 from __future__ import absolute_import
-import csv
 import daprot.mapper
 from .. import (
     Reader as AbstractReader,
@@ -11,11 +10,15 @@ from ..utils import *
 from ..compat import *
 from openpyxl import *
 
+
 class Reader(AbstractReader):
     # void
-    def __init__(self, resource, schema, offset=0, limit=None, sheet=None,  **fmtparams):
+    def __init__(self, resource, schema, offset=0, limit=None, sheet_name=None, merge_sheets=False, has_header=False, **fmtparams):
         self.fmtparams = fmtparams
-        self.sheet = sheet
+        self.sheet_name = sheet_name
+        # TODO merge functionality
+        self.merge_sheets = merge_sheets
+        self.has_header = has_header
         super(Reader, self).__init__(resource, schema, offset, limit)
 
     # function
@@ -24,29 +27,62 @@ class Reader(AbstractReader):
 
     def get_iterable_data(self):
         workbook = load_workbook(filename=BytesIO(encode(get_content(self.resource), self.encoding)))
-        worksheet = workbook[self.sheet] if self.sheet else workbook.active
 
-        result_gen = ([unicode(c.value or u'') for c in r] for r in worksheet.rows)
-        return result_gen
+        if self.merge_sheets:
+            worksheet = workbook.worksheets
+        else:
+            worksheet = [workbook[self.sheet_name] if self.sheet_name else workbook.active]
+
+        for ws in worksheet:
+            rows = iter(ws.rows)
+            if self.has_header:
+                next(rows)
+            for row in rows:
+                yield [unicode(cell.value or u'') for cell in row]
+
 
 class Writer(AbstractWriter):
     # void
-    def __init__(self, filename, iterable_data, input_schema=None, add_header=True, sheet=None, **fmtparams):
+    def __init__(self, filename, iterable_data, input_schema=None, add_header=True, sheet_name=None, replace_file = True, truncate_sheet = True, dinamic_sheet_field = None,  **fmtparams):
         self.fmtparams = fmtparams
         self.add_header = add_header
-        self.sheet = sheet
+        self.sheet_name = sheet_name
+        self.replace_file = replace_file
+        self.truncate_sheet = truncate_sheet
+        self.dinamic_sheet_field = dinamic_sheet_field
+        self.is_exists_file = os.path.isfile(filename)
+        self.is_exists_sheet = True
         super(Writer, self).__init__(filename, iterable_data, input_schema)
 
         if self.is_nested():
-            raise exceptions.NestedSchemaNotSupported("{self} is not support nested schemas." \
-                .format(self=self.name))
+            raise exceptions.NestedSchemaNotSupported("{self} is not support nested schemas.".format(self=self.name))
+
+    # openpyxl.Workbook
+    def _get_workbook(self):
+        if self.is_exists_file and not self.replace_file:
+            return load_workbook(self.resource)
+        return Workbook()
+
+    # openpyxl.Worksheet
+    def _get_worksheet(self, workbook):
+        if not self.sheet_name:
+            self.sheet_name = workbook.active.title
+            return workbook.active
+
+        if self.sheet_name in workbook.get_sheet_names():
+            return workbook.get_sheet_by_name(self.sheet_name)
+
+        self.is_exists_sheet = False
+        worksheet = workbook.create_sheet(title=self.sheet_name)
+        workbook.active = workbook.get_index(worksheet)
+        return worksheet
 
     # tuple
     def init_writer(self):
-        workbook = Workbook()
-        worksheet = workbook[self.sheet] if self.sheet else workbook.active
+        workbook = self._get_workbook()
+        worksheet = self._get_worksheet(workbook)
 
-        return (workbook, worksheet)
+        return workbook, worksheet
 
     # str in PY3 and unicode in PY2
     def unmarshal_item(self, item):
@@ -57,18 +93,55 @@ class Writer(AbstractWriter):
         return unicode(item or u'')
 
     # void
-    def write_header(self):
-        self.writer[1].append(self.fieldnames)
+    def write_header(self, worksheet):
+        if self.add_header and worksheet.max_row == 1:
+            worksheet.append(self.fieldnames)
+
+    # void
+    def _truncate_worksheet(self):
+        if not self.truncate_sheet:
+            return
+
+        if not self.is_exists_file:
+            return
+
+        if not self.is_exists_sheet:
+            return
+
+        self.writer[0].remove_sheet(self.writer[1])
+        self.writer = (self.writer[0], self.writer[0].create_sheet(title=self.sheet_name))
 
     # void
     def write(self):
-        if self.add_header: self.write_header()
+        self._truncate_worksheet()
+        self.write_header(self.writer[1])
         super(Writer, self).write()
         self.writer[0].save(self.resource)
+
+    # openpyxl.Worksheet
+    def _get_dinamic_sheet(self, worksheet_name):
+        if worksheet_name in self.writer[0].get_sheet_names():
+            return self.writer[0].get_sheet_by_name(worksheet_name)
+
+        new_worksheet = self.writer[0].create_sheet(title=worksheet_name)
+        self.write_header(new_worksheet)
+        return new_worksheet
 
     # void
     def write_item(self, item):
         data = unmarshal(item, self.unmarshal_item)
         self.writer[1].append([data[c] for c in self.fieldnames])
+
+        if not self.dinamic_sheet_field:
+            return
+
+        dinamic_sheet_name = data[self.dinamic_sheet_field]
+
+        if dinamic_sheet_name is None:
+            return
+
+        dinamic_sheet = self._get_dinamic_sheet(dinamic_sheet_name)
+        dinamic_sheet.append([data[c] for c in self.fieldnames])
+
 
 
--- riwo/tests/excel.py
+++ riwo/tests/excel.py
@@ -3,6 +3,7 @@
 from __future__ import absolute_import
 import os
 import io
+import shutil
 from openpyxl import load_workbook
 import riwo
 import daprot as dp
@@ -18,6 +19,7 @@ from . import (
     __dir__, 
     __remote__
 )
+
 
 class CommonReader(AbstractCommonReader):
     expected_result = [{
@@ -52,7 +54,41 @@ class CommonReader(AbstractCommonReader):
       u'id': u'P0005'
     }]
 
-class CommonWriter(AbstractCommonWrite, unittest.TestCase):
+
+class CommonMergeReader(AbstractCommonReader):
+    expected_result = [{
+      u'quantity': 1,
+      u'name': u'đói',
+      u'updated_at': u'2015.09.20 20:00',
+      u'price': u'449',
+      u'id': u'P0001'
+    }, {
+      u'quantity': 1,
+      u'name': u'배고픈',
+      u'updated_at': u'2015.09.20 20:02',
+      u'price': u'399',
+      u'id': u'P0002'
+    }, {
+      u'quantity': 10,
+      u'name': u'голодный',
+      u'updated_at': u'',
+      u'price': u'199',
+      u'id': u'P0003'
+    }, {
+      u'quantity': 1,
+      u'name': u'Űrállomás krízis',
+      u'updated_at': u'2015.09.20 12:47',
+      u'price': u'999.5',
+      u'id': u'P0004'
+    }, {
+      u'quantity': 1,
+      u'name': u'Ovális iroda',
+      u'updated_at': u'2015.09.20 07:31',
+      u'price': u'2 399',
+      u'id': u'P0005'
+    }]
+
+class CommonWriter(AbstractCommonWrite):
 
     def setUp(self):
         self.test_file_path = self.test_file_base \
@@ -77,6 +113,7 @@ class IndexSchema(dp.SchemaFlow):
     quantity = dp.Field(3, type=long)
     updated_at = dp.Field(4)
 
+
 class HeaderedSchema(dp.SchemaFlow):
     id = dp.Field()
     name = dp.Field(type=unicode, transforms=unicode.strip)
@@ -84,20 +121,59 @@ class HeaderedSchema(dp.SchemaFlow):
     quantity = dp.Field(type=long)
     updated_at = dp.Field()
 
+
 class LocalReader(CommonReader, unittest.TestCase):
     def setUp(self):
         self.resource = io.open(os.path.join(__dir__, 'test.xlsx'), 'rb')
         self.reader = riwo.excel.Reader(self.resource, IndexSchema, sheet='Sheet1')
 
-class RequestsReader(CommonReader, unittest.TestCase):
+
+class LocalReaderMerge(CommonReader, unittest.TestCase):
     def setUp(self):
-        self.resource = requests.get(os.path.join(__remote__, 'test.xlsx'))
-        self.reader = riwo.excel.Reader(self.resource, IndexSchema, sheet='Sheet1')
+        self.resource = io.open(os.path.join(__dir__, 'test-merge.xlsx'), 'rb')
+        self.reader = riwo.excel.Reader(self.resource, IndexSchema, merge_sheets=True)
+
+
+class LocalReaderMergeWithHeader(CommonReader, unittest.TestCase):
+    def setUp(self):
+        self.resource = io.open(os.path.join(__dir__, 'test-merge-with-header.xlsx'), 'rb')
+        self.reader = riwo.excel.Reader(self.resource, IndexSchema, merge_sheets=True, has_header=True)
+
+
+# class RequestsReader(CommonReader, unittest.TestCase):
+#     def setUp(self):
+#         self.resource = requests.get(os.path.join(__remote__, 'test.xlsx'))
+#         self.reader = riwo.excel.Reader(self.resource, IndexSchema, sheet='Sheet1')
+
+
+class RequestsReaderMerge(CommonReader, unittest.TestCase):
+    def setUp(self):
+        self.resource = requests.get(os.path.join(__remote__, 'test-merge.xlsx'))
+        self.reader = riwo.excel.Reader(self.resource, IndexSchema, merge_sheets=True)
+
+
+class RequestsReaderMergeWithHeader(CommonReader, unittest.TestCase):
+    def setUp(self):
+        self.resource = requests.get(os.path.join(__remote__, 'test-merge-with-header.xlsx'))
+        self.reader = riwo.excel.Reader(self.resource, IndexSchema, merge_sheets=True, has_header=True)
+
 
 class UrllibReader(CommonReader, unittest.TestCase):
     def setUp(self):
         self.resource = urlopen(os.path.join(__remote__, 'test.xlsx'))
         self.reader = riwo.excel.Reader(self.resource, IndexSchema, sheet='Sheet1')
+
+
+class UrllibReaderMerge(CommonReader, unittest.TestCase):
+    def setUp(self):
+        self.resource = urlopen(os.path.join(__remote__, 'test-merge.xlsx'))
+        self.reader = riwo.excel.Reader(self.resource, IndexSchema, merge_sheets=True)
+
+
+class UrllibReaderMergeWithHeader(CommonReader, unittest.TestCase):
+    def setUp(self):
+        self.resource = urlopen(os.path.join(__remote__, 'test-merge-with-header.xlsx'))
+        self.reader = riwo.excel.Reader(self.resource, IndexSchema, merge_sheets=True, has_header=True)
 
 class LocalWriter(CommonWriter, unittest.TestCase):
     class Schema(dp.SchemaFlow):
@@ -184,3 +260,240 @@ class LocalWriter(CommonWriter, unittest.TestCase):
              riwo.excel.Writer(self.resource, [], self.NestedSchema)
         self.create_file = False
         self.content = u''
+
+
+class CommonOptionWriter(AbstractCommonWrite):
+    iterable_input = CommonReader.expected_result
+    test_file_base = os.path.join(__dir__, 'output-{token}.{ext}')
+    ext = 'xlsx'
+    test_file_name = 'test.xlsx'
+    sheet_name = None
+
+    def setUp(self):
+        self.test_file_path = self.test_file_base \
+            .format(ext=self.ext, token=''.join([random.choice(string.ascii_uppercase) for i in range(6)]))
+        shutil.copyfile(os.path.join(__dir__, self.test_file_name), self.test_file_path)
+        self.resource = self.test_file_path
+        self.dinamic_content = {}
+
+    def tearDown(self):
+        workbook = load_workbook(self.test_file_path)
+        worksheet = workbook[self.sheet_name] if self.sheet_name else workbook.active
+        content = [[unicode(c.value or u'') for c in r] for r in worksheet.rows]
+        self.assertEqual(self.content, content)
+
+        for sheet_name, sheet_content in list(self.dinamic_content.items()):
+            self.assertEqual(
+                sheet_content,
+                [[unicode(c.value or u'') for c in r] for r in workbook[sheet_name].rows]
+            )
+
+        os.remove(self.test_file_path)
+
+
+class LocalOptionWriter(CommonOptionWriter, unittest.TestCase):
+    class Schema(dp.SchemaFlow):
+        id = dp.Field()
+        name = dp.Field()
+        price = dp.Field()
+        quantity = dp.Field()
+        updated_at = dp.Field()
+
+    test_file_name = 'test-option.xlsx'
+    sheet_name = None
+
+    def test_not_truncate_not_add_header(self):
+        self.writer = riwo.excel.Writer(
+            self.resource,
+            self.iterable_input,
+            self.Schema,
+            sheet_name=self.sheet_name,
+            add_header=False,
+            truncate_sheet=False,
+            replace_file=False
+        )
+        self.writer.write()
+        self.content = [
+            [u'P0006', u'đói', u'449', u'1', u'2015.09.20 20:00'],
+            [u'P0007', u'배고픈', u'399', u'1', u'2015.09.20 20:02'],
+            [u'P0008', u'голодный', u'199', u'10', u''],
+            [u'P0009', u'Űrállomás krízis', u'999.5', u'1', u'2015.09.20 12:47'],
+            [u'P0010', u'Ovális iroda', u'2 399', u'1', u'2015.09.20 07:31'],
+            [u'P0001', u'đói', u'449', u'1', u'2015.09.20 20:00'],
+            [u'P0002', u'배고픈', u'399', u'1', u'2015.09.20 20:02'],
+            [u'P0003', u'голодный', u'199', u'10', u''],
+            [u'P0004', u'Űrállomás krízis', u'999.5', u'1', u'2015.09.20 12:47'],
+            [u'P0005', u'Ovális iroda', u'2 399', u'1', u'2015.09.20 07:31'],
+        ]
+
+    def test_not_truncate_add_header(self):
+        self.writer = riwo.excel.Writer(
+            self.resource,
+            self.iterable_input,
+            self.Schema,
+            sheet_name=self.sheet_name,
+            add_header=True,
+            truncate_sheet=False,
+            replace_file=False
+        )
+        self.writer.write()
+        self.content = [
+            [u'P0006', u'đói', u'449', u'1', u'2015.09.20 20:00'],
+            [u'P0007', u'배고픈', u'399', u'1', u'2015.09.20 20:02'],
+            [u'P0008', u'голодный', u'199', u'10', u''],
+            [u'P0009', u'Űrállomás krízis', u'999.5', u'1', u'2015.09.20 12:47'],
+            [u'P0010', u'Ovális iroda', u'2 399', u'1', u'2015.09.20 07:31'],
+            [u'P0001', u'đói', u'449', u'1', u'2015.09.20 20:00'],
+            [u'P0002', u'배고픈', u'399', u'1', u'2015.09.20 20:02'],
+            [u'P0003', u'голодный', u'199', u'10', u''],
+            [u'P0004', u'Űrállomás krízis', u'999.5', u'1', u'2015.09.20 12:47'],
+            [u'P0005', u'Ovális iroda', u'2 399', u'1', u'2015.09.20 07:31'],
+        ]
+
+    def test_truncate_not_add_header(self):
+        self.writer = riwo.excel.Writer(
+            self.resource,
+            self.iterable_input,
+            self.Schema,
+            sheet_name=self.sheet_name,
+            add_header=False,
+            truncate_sheet=True,
+            replace_file=False
+        )
+        self.writer.write()
+        self.content = [
+            [u'P0001', u'đói', u'449', u'1', u'2015.09.20 20:00'],
+            [u'P0002', u'배고픈', u'399', u'1', u'2015.09.20 20:02'],
+            [u'P0003', u'голодный', u'199', u'10', u''],
+            [u'P0004', u'Űrállomás krízis', u'999.5', u'1', u'2015.09.20 12:47'],
+            [u'P0005', u'Ovális iroda', u'2 399', u'1', u'2015.09.20 07:31'],
+        ]
+
+    def test_truncate_add_header(self):
+        self.writer = riwo.excel.Writer(
+            self.resource,
+            self.iterable_input,
+            self.Schema,
+            sheet_name=self.sheet_name,
+            add_header=True,
+            truncate_sheet=True,
+            replace_file=False
+        )
+        self.writer.write()
+        self.content = [
+            [u'id', u'name', u'price', u'quantity', u'updated_at'],
+            [u'P0001', u'đói', u'449', u'1', u'2015.09.20 20:00'],
+            [u'P0002', u'배고픈', u'399', u'1', u'2015.09.20 20:02'],
+            [u'P0003', u'голодный', u'199', u'10', u''],
+            [u'P0004', u'Űrállomás krízis', u'999.5', u'1', u'2015.09.20 12:47'],
+            [u'P0005', u'Ovális iroda', u'2 399', u'1', u'2015.09.20 07:31'],
+        ]
+
+    def test_replace_not_add_header(self):
+        self.writer = riwo.excel.Writer(
+            self.resource,
+            self.iterable_input,
+            self.Schema,
+            sheet_name=self.sheet_name,
+            add_header=False,
+            truncate_sheet=False,
+            replace_file=True
+        )
+        self.writer.write()
+        self.content = [
+            [u'P0001', u'đói', u'449', u'1', u'2015.09.20 20:00'],
+            [u'P0002', u'배고픈', u'399', u'1', u'2015.09.20 20:02'],
+            [u'P0003', u'голодный', u'199', u'10', u''],
+            [u'P0004', u'Űrállomás krízis', u'999.5', u'1', u'2015.09.20 12:47'],
+            [u'P0005', u'Ovális iroda', u'2 399', u'1', u'2015.09.20 07:31'],
+        ]
+
+    def test_replace_add_header(self):
+        self.writer = riwo.excel.Writer(
+            self.resource,
+            self.iterable_input,
+            self.Schema,
+            sheet_name=self.sheet_name,
+            add_header=True,
+            truncate_sheet=False,
+            replace_file=True
+        )
+        self.writer.write()
+        self.content = [
+            [u'id', u'name', u'price', u'quantity', u'updated_at'],
+            [u'P0001', u'đói', u'449', u'1', u'2015.09.20 20:00'],
+            [u'P0002', u'배고픈', u'399', u'1', u'2015.09.20 20:02'],
+            [u'P0003', u'голодный', u'199', u'10', u''],
+            [u'P0004', u'Űrállomás krízis', u'999.5', u'1', u'2015.09.20 12:47'],
+            [u'P0005', u'Ovális iroda', u'2 399', u'1', u'2015.09.20 07:31'],
+        ]
+
+    def test_dinamic_sheet_field_not_add_header(self):
+        self.writer = riwo.excel.Writer(
+            self.resource,
+            self.iterable_input,
+            self.Schema,
+            sheet_name=self.sheet_name,
+            add_header=False,
+            truncate_sheet=False,
+            replace_file=True,
+            dinamic_sheet_field=u'quantity'
+        )
+        self.writer.write()
+        self.content = [
+            [u'P0001', u'đói', u'449', u'1', u'2015.09.20 20:00'],
+            [u'P0002', u'배고픈', u'399', u'1', u'2015.09.20 20:02'],
+            [u'P0003', u'голодный', u'199', u'10', u''],
+            [u'P0004', u'Űrállomás krízis', u'999.5', u'1', u'2015.09.20 12:47'],
+            [u'P0005', u'Ovális iroda', u'2 399', u'1', u'2015.09.20 07:31'],
+        ]
+        self.dinamic_content = {
+            u'1': [
+                [u'P0001', u'đói', u'449', u'1', u'2015.09.20 20:00'],
+                [u'P0002', u'배고픈', u'399', u'1', u'2015.09.20 20:02'],
+                [u'P0004', u'Űrállomás krízis', u'999.5', u'1', u'2015.09.20 12:47'],
+                [u'P0005', u'Ovális iroda', u'2 399', u'1', u'2015.09.20 07:31'],
+            ],
+            u'10': [
+                [u'P0003', u'голодный', u'199', u'10', u''],
+            ]
+        }
+
+    def test_dinamic_sheet_field_add_header(self):
+        self.writer = riwo.excel.Writer(
+            self.resource,
+            self.iterable_input,
+            self.Schema,
+            sheet_name=self.sheet_name,
+            add_header=True,
+            truncate_sheet=False,
+            replace_file=True,
+            dinamic_sheet_field=u'quantity'
+        )
+        self.writer.write()
+        self.content = [
+            [u'id', u'name', u'price', u'quantity', u'updated_at'],
+            [u'P0001', u'đói', u'449', u'1', u'2015.09.20 20:00'],
+            [u'P0002', u'배고픈', u'399', u'1', u'2015.09.20 20:02'],
+            [u'P0003', u'голодный', u'199', u'10', u''],
+            [u'P0004', u'Űrállomás krízis', u'999.5', u'1', u'2015.09.20 12:47'],
+            [u'P0005', u'Ovális iroda', u'2 399', u'1', u'2015.09.20 07:31'],
+        ]
+        self.dinamic_content = {
+            u'1': [
+                [u'id', u'name', u'price', u'quantity', u'updated_at'],
+                [u'P0001', u'đói', u'449', u'1', u'2015.09.20 20:00'],
+                [u'P0002', u'배고픈', u'399', u'1', u'2015.09.20 20:02'],
+                [u'P0004', u'Űrállomás krízis', u'999.5', u'1', u'2015.09.20 12:47'],
+                [u'P0005', u'Ovális iroda', u'2 399', u'1', u'2015.09.20 07:31'],
+            ],
+            u'10': [
+                [u'id', u'name', u'price', u'quantity', u'updated_at'],
+                [u'P0003', u'голодный', u'199', u'10', u''],
+            ]
+        }
+
+
+class LocalOptionWriterWithSheetName(LocalOptionWriter, unittest.TestCase):
+
+    sheet_name = u'Sheet1'
